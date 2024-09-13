813a6d25651f710fea2f570cdec431691624cd18
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-09-13 14:42:04 +0600

    Project Initial Commit

---
+++ app/Models/Student.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'class_id',
+        'section_id',
+        'name',
+        'email',
+        'image'
+    ];
+
+    use HasFactory;
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+}
